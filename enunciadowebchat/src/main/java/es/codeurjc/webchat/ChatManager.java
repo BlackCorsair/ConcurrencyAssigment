72ab8c14d5cc8ebd00f1087e9dabc9b13285c0a3
Jorge Hevia <jmhev@outlook.com>
2020-01-30 20:36:51 +0100

    Reindent ChatManager

--- enunciadowebchat/src/main/java/es/codeurjc/webchat/ChatManager.java
+++ enunciadowebchat/src/main/java/es/codeurjc/webchat/ChatManager.java
@@ -9,72 +9,73 @@ import java.util.concurrent.TimeoutException;
 
 public class ChatManager {
 
-	private Map<String, Chat> chats = Collections.synchronizedMap(new HashMap<>());
-	private Map<String, User> users = Collections.synchronizedMap(new HashMap<>());
-	private int maxChats;
+    private Map<String, Chat> chats = Collections.synchronizedMap(new HashMap<>());
+    private Map<String, User> users = Collections.synchronizedMap(new HashMap<>());
+    private int maxChats;
 
-	public ChatManager(int maxChats) {
-		this.maxChats = maxChats;
-	}
+    public ChatManager(int maxChats) {
+        this.maxChats = maxChats;
+    }
 
-	public void newUser(User user) {
-		
-		if(users.containsKey(user.getName())){
-			throw new IllegalArgumentException("There is already a user with name \'"
-					+ user.getName() + "\'");
-		} else {
-			users.put(user.getName(), user);
-		}
-	}
+    public void newUser(User user) {
 
-	public Chat newChat(String name, long timeout, TimeUnit unit) throws InterruptedException,
-			TimeoutException {
+        if (users.containsKey(user.getName())) {
+            throw new IllegalArgumentException("There is already a user with name \'"
+                    + user.getName() + "\'");
+        } else {
+            users.put(user.getName(), user);
+        }
+    }
 
-		if (chats.size() == maxChats) {
-			throw new TimeoutException("There is no enought capacity to create a new chat");
-		}
+    public Chat newChat(String name, long timeout, TimeUnit unit) throws InterruptedException,
+            TimeoutException {
 
-		if(chats.containsKey(name)){
-			return chats.get(name);
-		} else {
-			Chat newChat = new Chat(this, name);
-			chats.put(name, newChat);
-			
-			for(User user : users.values()){
-				user.newChat(newChat);
-			}
+        if (chats.size() == maxChats) {
+            throw new TimeoutException("There is no enought capacity to create a new chat");
+        }
 
-			return newChat;
-		}
-	}
+        if (chats.containsKey(name)) {
+            return chats.get(name);
+        } else {
+            Chat newChat = new Chat(this, name);
+            chats.put(name, newChat);
 
-	public void closeChat(Chat chat) {
-		Chat removedChat = chats.remove(chat.getName());
-		if (removedChat == null) {
-			throw new IllegalArgumentException("Trying to remove an unknown chat with name \'"
-					+ chat.getName() + "\'");
-		}
+            for (User user : users.values()) {
+                user.newChat(newChat);
+            }
 
-		for(User user : users.values()){
-			user.chatClosed(removedChat);
-		}
-	}
+            return newChat;
+        }
+    }
 
-	public Collection<Chat> getChats() {
-		return Collections.unmodifiableCollection(chats.values());
-	}
+    public void closeChat(Chat chat) {
+        Chat removedChat = chats.remove(chat.getName());
+        if (removedChat == null) {
+            throw new IllegalArgumentException("Trying to remove an unknown chat with name \'"
+                    + chat.getName() + "\'");
+        }
 
-	public Chat getChat(String chatName) {
-		return chats.get(chatName);
-	}
+        for (User user : users.values()) {
+            user.chatClosed(removedChat);
+        }
+    }
 
-	public Collection<User> getUsers() {
-		return Collections.unmodifiableCollection(users.values());
-	}
+    public Collection<Chat> getChats() {
+        return Collections.unmodifiableCollection(chats.values());
+    }
 
-	public User getUser(String userName) {
-		return users.get(userName);
-	}
+    public Chat getChat(String chatName) {
+        return chats.get(chatName);
+    }
 
-	public void close() {}
+    public Collection<User> getUsers() {
+        return Collections.unmodifiableCollection(users.values());
+    }
+
+    public User getUser(String userName) {
+        return users.get(userName);
+    }
+
+    public void close() {
+    }
 }
