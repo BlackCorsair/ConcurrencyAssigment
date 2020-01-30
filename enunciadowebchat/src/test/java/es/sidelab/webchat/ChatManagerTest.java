c7d72a3f2e3ec68a25897c044119c68b64698f42
Jorge Hevia <jmhev@outlook.com>
2020-01-30 20:38:47 +0100

    Reindent ChatManagerTest

--- enunciadowebchat/src/test/java/es/sidelab/webchat/ChatManagerTest.java
+++ enunciadowebchat/src/test/java/es/sidelab/webchat/ChatManagerTest.java
@@ -14,55 +14,55 @@ import es.codeurjc.webchat.User;
 
 public class ChatManagerTest {
 
-	@Test
-	public void newChat() throws InterruptedException, TimeoutException {
+    @Test
+    public void newChat() throws InterruptedException, TimeoutException {
 
-		// Crear el chat Manager
-		ChatManager chatManager = new ChatManager(5);
+        // Crear el chat Manager
+        ChatManager chatManager = new ChatManager(5);
 
-		// Crear un usuario que guarda en chatName el nombre del nuevo chat
-		final String[] chatName = new String[1];
+        // Crear un usuario que guarda en chatName el nombre del nuevo chat
+        final String[] chatName = new String[1];
 
-		chatManager.newUser(new TestUser("user") {
-			public void newChat(Chat chat) {
-				chatName[0] = chat.getName();
-			}
-		});
+        chatManager.newUser(new TestUser("user") {
+            public void newChat(Chat chat) {
+                chatName[0] = chat.getName();
+            }
+        });
 
-		// Crear un nuevo chat en el chatManager
-		chatManager.newChat("Chat2", 5, TimeUnit.SECONDS);
+        // Crear un nuevo chat en el chatManager
+        chatManager.newChat("Chat2", 5, TimeUnit.SECONDS);
 
-		// Comprobar que el chat recibido en el método 'newChat' se llama 'Chat'
-		assertTrue("The method 'newChat' should be invoked with 'Chat', but the value is "
-				+ chatName[0], Objects.equals(chatName[0], "Chat"));
-	}
+        // Comprobar que el chat recibido en el método 'newChat' se llama 'Chat'
+        assertTrue("The method 'newChat' should be invoked with 'Chat', but the value is "
+                + chatName[0], Objects.equals(chatName[0], "Chat"));
+    }
 
-	@Test
-	public void newUserInChat() throws InterruptedException, TimeoutException {
+    @Test
+    public void newUserInChat() throws InterruptedException, TimeoutException {
 
-		ChatManager chatManager = new ChatManager(5);
+        ChatManager chatManager = new ChatManager(5);
 
-		final String[] newUser = new String[1];
+        final String[] newUser = new String[1];
 
-		TestUser user1 = new TestUser("user1") {
-			@Override
-			public void newUserInChat(Chat chat, User user) {
-				newUser[0] = user.getName();
-			}
-		};
+        TestUser user1 = new TestUser("user1") {
+            @Override
+            public void newUserInChat(Chat chat, User user) {
+                newUser[0] = user.getName();
+            }
+        };
 
-		TestUser user2 = new TestUser("user2");
+        TestUser user2 = new TestUser("user2");
 
-		chatManager.newUser(user1);
-		chatManager.newUser(user2);
+        chatManager.newUser(user1);
+        chatManager.newUser(user2);
 
-		Chat chat = chatManager.newChat("Chat", 5, TimeUnit.SECONDS);
+        Chat chat = chatManager.newChat("Chat", 5, TimeUnit.SECONDS);
 
-		chat.addUser(user1);
-		chat.addUser(user2);
+        chat.addUser(user1);
+        chat.addUser(user2);
 
-		assertTrue("Notified new user '" + newUser[0] + "' is not equal than user name 'user2'",
-				"user2".equals(newUser[0]));
+        assertTrue("Notified new user '" + newUser[0] + "' is not equal than user name 'user2'",
+                "user2".equals(newUser[0]));
 
-	}
+    }
 }
